3bdf6b0ee251b5fd5fc03408ce48a128d31382ad
VaskarO <contact2vo@gmail.com>
2024-09-02 02:57:45 +0200

    user signin ui and functionality

--- Property-offer-app/frontend/src/pages/Signin.jsx
+++ Property-offer-app/frontend/src/pages/Signin.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Signin = () => {
+  const navigate = useNavigate()
+  const [signinData, setSigninData] = useState({})
+  
+  const handleChange = (e)=>{
+    return setSigninData({
+      ...signinData,
+      [e.target.id]:e.target.value
+    })
+  }
+  const handleSubmit = async (e)=>  {
+    e.preventDefault();
+    try{
+      console.log(signinData)
+      const res = await fetch('/api/auth/signin',{
+        method:'POST',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(signinData)
+      });
+      const data = await res.json()
+      console.log(data)
+
+      if(data.success === false){
+        console.log('LOGIN FAILED')
+      }
+      navigate('/profile')
+    }catch(error){
+      console.log(error)
+    }
+
+  }
   return (
-    <div>Signin</div>
+    <div className='flex flex-col mx-auto my-auto pt-15 justify-center min-h-screen bg-slate-300 items-center'>
+    <h1 className='pb-10 text-3xl center text font-bold ' >Sign In</h1>
+    <form onSubmit={handleSubmit} action="" className=' w-1/3 flex flex-col'>
+      <input onChange={handleChange} type="text " id= "email" className ="mb-6 rounded-md font-normal  px-3 py-2 " placeholder='Email' />
+      <input onChange={handleChange} type="text " id='password' className ="mb-6 rounded-md font-normal  px-3 py-2 " placeholder='Password' />
+      {/* <input onChange={handleChange} type="text " className ="mb-6 rx rounded-md font-normal  px-3 py-2 " placeholder='Confirm password' /> */}
+      <button className='hover:bg-blue-900 w-full bg-blue-950 text-white py-3 font-bold rounded-md'> Signin now</button>
+    </form>
+  </div>
   )
 }
 
