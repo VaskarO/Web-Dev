fa5f572501ddbfacb899dc1ef6e07e32597d457f
VaskarO <contact2vo@gmail.com>
2024-09-07 18:20:22 +0200

    update advertisement controller

--- Property-offer-app/api/controller/advertiseList.controller.js
+++ Property-offer-app/api/controller/advertiseList.controller.js
@@ -1,20 +1,47 @@
 import { AdveriseList } from "../models/Advertise.model.js"
 import { handleError } from "../utils/handleError.js";
-import { verifyUser } from "../utils/verifyUser.js"
 
 export const createAdvertise = async(req, res, next)=>{
     const userId = req.userId
     const {advertiseType,title,title1,description,price,numberOfRooms,floorNumber,haveBathroom,availableFrom,petAllowed,haveBalcony} = req.body;
 
     const newAdvertiseList = new AdveriseList({advertiseType,title,title1,description,price,numberOfRooms,floorNumber,haveBathroom,availableFrom,petAllowed,haveBalcony, createdBy:userId})
-    console.log(userId)
     try{
         await newAdvertiseList.save()
         res.status(200).json({'message':'Advertisement created!!',"success":true, "AdvertisementId": newAdvertiseList._id})
-        console.log()
 
     }catch(err){
         next(handleError(500,err.message))
     }
 
+}
+
+export const updateAdvertisement = async (req, res, next)=>{
+    const currentUserId = req.userId;
+
+    const getAdvertiseById = await AdveriseList.findById({_id:req.params.id})
+    // console.log(getAdvertiseById.createdBy)
+    if(currentUserId !== getAdvertiseById.createdBy) {return next(handleError(403, 'Unauthorized request!'))}
+
+    try{
+        const updateAdvertise = await AdveriseList.findByIdAndUpdate(req.params.id, {
+            $set:{
+                advertiseType:req.body.advertiseType,
+                title:req.body.title,
+                description:req.body.description,
+                price:req.body.price,
+                numberOfRooms:req.body.numberOfRooms,
+                floorNumber:req.body.floorNumber,
+                haveBathroom:req.body.haveBathroom,
+                availableFrom:req.body.availableFrom,
+                petAllowed: req.body.petAllowed,
+                haveBalcony:req.body.haveBalcony,
+                }
+            },{new:true})
+            // res.status(200).json({'message':"updated"})
+            res.status(200).json({'success':'true', 'message':`Advertisement with ID ${req.params.id} is successfully updated`, 'advertisement':updateAdvertise})
+            
+    }catch(err){
+        next(handleError(500, err.message))
+    }
 }
--- Property-offer-app/api/routes/advertiseList.route.js
+++ Property-offer-app/api/routes/advertiseList.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {createAdvertise} from '../controller/advertiseList.controller.js'
+import {createAdvertise, updateAdvertisement} from '../controller/advertiseList.controller.js'
 import { verifyUser } from "../utils/verifyUser.js";
 import { getCurrentUser } from "../utils/getCurrentUser.js";
 
 const advertiseListRoute = express.Router()
 
 advertiseListRoute.post('/create',getCurrentUser, createAdvertise)
+advertiseListRoute.post('/update/:id',getCurrentUser, updateAdvertisement)
 
 export default advertiseListRoute
