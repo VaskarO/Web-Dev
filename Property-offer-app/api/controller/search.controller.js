1cc6807bb826017fa84a83e1edacfd6e476e720f
VaskarO <contact2vo@gmail.com>
2024-09-19 05:05:53 +0200

    pagination api with limit and page

--- Property-offer-app/api/controller/search.controller.js
+++ Property-offer-app/api/controller/search.controller.js
@@ -5,35 +5,32 @@ import { handleError } from "../utils/handleError.js";
 export const searchAdvertisement = async (req, res)=>{
     try{
         const query = req.query.searchQuery ||""
+        const limit = req.query.limit || '3'
+        const page = req.query.page || '1'
         if(!query){
             res.status(200).json({"message":"Search item not provided"})
         }
         if(query.length<5){
             res.status(200).json({'message':"Atleast 4 letter required"})
         }
-    //     const results = await AdvertiseList.aggregate([
-    //         {
-    //             $search: {
-    //             text:{
-    //                 query:'tite',
-    //                 path:"title",
 
-    //             }
-    //         }
-    //     },
-    //     {
-            
-    //         $project:{
-    //         _id:1,
-    //         title:1,
-    //     }
-    // }
-    // ])
-        let results = await AdvertiseList.find({ title:  new RegExp(query, 'i'),  } )
-        results.push(...results, ...await AdvertiseList.find({ description:  new RegExp(query, 'i'),  } ))
+        const skip = (page-1)*limit
+        const queryRegx = new RegExp(query, 'i')
+        const results = await AdvertiseList.find({
+            $or: [
+                { title: { $regex: queryRegx } },
+                { description: { $regex: queryRegx } },
+            ],
+            }).limit(limit).skip(skip)
+        
+        if(req.query.page){
+            const advertiseCount = await AdvertiseList.countDocuments();
+            if(skip>=advertiseCount){
+                handleError(403, "Page not found.")
+            }
+        }
+        
 
-        console.log(results.length)
-        console.log(results)
         if(results.length===0){
             res.status(200).json({searchList: "No search result"})
         }
