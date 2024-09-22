3496d8ef8a331f48e76a0bb8f053934c70efe3eb
VaskarO <contact2vo@gmail.com>
2024-09-22 23:54:07 +0200

    pagination page change and currentPage update

--- Property-offer-app/frontend/src/components/Pagination.jsx
+++ Property-offer-app/frontend/src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Pagination = ({pages}) => {
+const Pagination = ({pages,currentPage, handlePageChange}) => {
     const pageList = []
     for (let i=1; i<=pages;i++){
         pageList.push(i)
@@ -9,17 +9,19 @@ const Pagination = ({pages}) => {
     <nav>
         <ul className="inline-flex -space-x-px text-sm my-10">
         <li>
-            <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+            <a onClick={()=>handlePageChange(currentPage>1?currentPage-1:1)}  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
         </li>
         {pageList.map((page, index)=>(
-            <li>
-            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</a>
+            <li  key={index}>
+            <a onClick={()=>handlePageChange(page)} className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 ${page ===currentPage?
+            'bg-blue-300':'bg-white'
+            } border  border-gray-300 hover:bg-gray-100 hover:text-gray-700`}>{page}</a>
             </li>
         ))}
            
 
         <li>
-            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+            <a onClick={()=>handlePageChange(currentPage< pageList[pageList.length-1]?currentPage+1:pageList[pageList.length-1])} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
         </li>
         </ul>
     </nav>
--- Property-offer-app/frontend/src/pages/Search.jsx
+++ Property-offer-app/frontend/src/pages/Search.jsx
@@ -1,26 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import SearchComponent from '../components/SearchComponent'
-import { useParams } from 'react-router-dom'
+import { useParams,useSearchParams } from 'react-router-dom'
 import SearchBar from '../components/SearchBar'
 import Pagination from '../components/Pagination'
 const Search = () => {
 
   const {query} = useParams()
+  const [searchParams] = useSearchParams()
   const [advertise, setAdvertise] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPage, setTotalPage] = useState(1)
   const [totalData, setTotalData] = useState(0)
 
-  const limit = 5
+
+  const limit = 4
   const pages =Math.ceil(totalData/limit)
   // console.log(id)
   useEffect(()=>{
-    fetch(`/api/search?searchQuery=${query}`)
+    fetch(`/api/search?searchQuery=${query}&page=${currentPage}&limit=${limit}`)
     .then(response => response.json())
     .then(data =>(setAdvertise(data.searchList), setTotalData(data.dataCount)))
     .catch(error=>console.log(error))
-  }, [query])
+  }, [query,currentPage])
   console.log(totalData)
+  console.log(currentPage)
   if(advertise === 'No search result'){
     return (
       <div className='flex flex-col  mt-10 items-center justify-center mx-auto'>
@@ -30,9 +33,15 @@ const Search = () => {
       </div>
     )
   }
+
+  const handlePageChange =(e)=>{
+     setCurrentPage(e);
+  }
+
   return (
     <div className='flex flex-col  mt-10 items-center justify-center mx-auto'>
     <SearchBar/>
+
     <div className='flex flex-col items-center text-center justify-center mx-auto'>
         <div className='text-3xl text-center mt-10 font-semibold text-blue-900'>Your search result for <i className='text-blue-600'>"{query}"</i></div>
         <div className='flex gap-5 items-center mx-auto justify-around mt-10'>
@@ -42,7 +51,7 @@ const Search = () => {
         </div>
     </div>
 
-    <Pagination pages={pages}/>
+    <Pagination pages={pages} currentPage= {currentPage} handlePageChange= {handlePageChange}/>
     </div>
   )
 }
