ecbcb021aaf3c0e87b5ee68d359919fe7d9070d1
VaskarO <contact2vo@gmail.com>
2024-09-24 22:22:04 +0200

    user profile page ui

--- Property-offer-app/frontend/src/pages/Profile.jsx
+++ Property-offer-app/frontend/src/pages/Profile.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import thumbnail from '../assets/images/300x200.png'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='flex flex-wrap w-4/5 mx-auto'>
+      <div className='w-1/2 bg-slate-300 h-screen'>
+        <div className='p-10 flex flex-col'>
+        <img className="rounded-lg shadow-lg w-2/3 h-full  object-cover" src={thumbnail} alt="" />
+        <p className='mt-5 text-lg font-semibold'>Fullname: <i className='text-blue-900 text-lg font-normal'>UserFullname</i> </p>
+        <p className='mt-5 text-lg font-semibold'>Email: <i className='text-blue-900 text-lg font-normal'>user@email.com</i> </p>
+        <p className='mt-5 text-lg font-semibold'>Phone: <i className='text-blue-900 text-lg font-normal'>+1 0000000000</i> </p>
+        <p className='mt-5 text-lg font-semibold'>Address: <i className='text-blue-900 text-lg font-normal'>Street nr 1, City, State, Country</i> </p>
+        <button className='px-3 py-2 bg-blue-900 text-white shadow-lg w-1/2 mt-5 font-semibold hover:bg-blue-700'>Update profile</button>
+        </div>
+      </div>
+      
+      <div className='w-1/2 bg-slate-200 h-screen flex flex-col items-center justify-center'>
+      <button className='px-8 py-3 bg-green-800 text-white shadow-lg w-1/2 mt-5 font-semibold hover:bg-green-600'>View Dashboard</button>
+      <button className='px-8 py-3 bg-green-800 text-white shadow-lg w-1/2 mt-5 font-semibold hover:bg-green-600'>Create Advertisement</button>
+      <button className='px-8 py-3 bg-green-800 text-white shadow-lg w-1/2 mt-5 font-semibold hover:bg-green-700'>Manage Advertisements</button>
+      <button className='px-8 py-3 bg-green-800 text-white shadow-lg w-1/2 mt-5 font-semibold hover:bg-green-700'>Manage Appointments</button>
+      </div>
+
+    </div>
   )
 }
 
