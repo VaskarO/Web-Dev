d6b22694ee37d6d57e8cb13a744ba8bc92ab897c
VaskarO <contact2vo@gmail.com>
2024-09-29 20:12:06 +0200

    conditional rendering as per logged in user

--- Property-offer-app/frontend/src/components/AdvertisementCard.jsx
+++ Property-offer-app/frontend/src/components/AdvertisementCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 import thumbnail from '../assets/images/300x200.png'
 import { FaMapMarkerAlt  } from 'react-icons/fa'
-
+import UserContext from '../contexts/UserContext'
 const AdvertisementCard = ({item}) => {
+  const {loggedIn} = useContext(UserContext)
+  const {setLoggedIn} = useContext(UserContext)
+  const {setUserInfo} = useContext(UserContext)
+  const [userId, setUserId] = useState(null)
+  useEffect(()=>{
+    fetch(`/api/auth/getUser`)
+    .then(response => response.json())
+    .then(data =>{data._id?(setUserInfo(data), setLoggedIn(true)):"", setUserId(data._id)} )
+    .catch(error=>console.log(error.message))
+  }, [loggedIn])
+
 
   return (
 <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 bg-slate-200 p-4 shadow-md rounded-lg">
@@ -22,6 +33,21 @@ const AdvertisementCard = ({item}) => {
       More details
     </button>
   </Link>
+  {loggedIn && userId === item.createdBy && (
+  <>
+        <Link to={`/adDetails/${item._id}`}>
+    <button className="bg-green-900 text-white w-full mt-3 py-2 rounded-md font-semibold hover:bg-green-800 hover:text-gray-200">
+      Edit Advertisement
+    </button>
+  </Link>
+    <Link to={`/adDetails/${item._id}`}>
+    <button className="bg-red-900 text-white w-full mt-3 py-2 rounded-md font-semibold hover:bg-red-800 hover:text-gray-200">
+      Remove Advertisement
+    </button>
+  </Link>
+  </>
+  
+  )}
 </div>
 
   )
