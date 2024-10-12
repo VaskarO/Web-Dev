8c904d34cbd2cc2f7cbe97141885cef61fa67d7c
VaskarO <contact2vo@gmail.com>
2024-10-12 21:32:49 +0200

    media in django

--- Django_app/project01/posts/models.py
+++ Django_app/project01/posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(default='blank.png', blank=True)
 
     def __str__(self):
         return self.title
--- Django_app/project01/project01/urls.py
+++ Django_app/project01/project01/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
