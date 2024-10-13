50665d0935d3c235731ed071613000e817ae77a8
VaskarO <contact2vo@gmail.com>
2024-10-13 20:42:53 +0200

    users app init

--- Django_app/project01/project01/urls.py
+++ Django_app/project01/project01/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home),
     path("posts/", include('posts.urls')),
+    path("users/", include('users.urls')),
 
 
     path("__reload__/", include("django_browser_reload.urls")),
