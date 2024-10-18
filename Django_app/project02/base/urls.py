637dd241a847a81d4cce8a68f2c16fc8e60daf8a
VaskarO <contact2vo@gmail.com>
2024-10-18 20:38:34 +0200

    app and routes

--- Django_app/project02/base/urls.py
+++ Django_app/project02/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('group/', views.group, name= "group")
+    path('group/', views.group, name= "group"),
+    path('group/<str:key>', views.group_by_id, name= "group_by_id")
 ]
--- Django_app/project02/base/views.py
+++ Django_app/project02/base/views.py
@@ -7,4 +7,14 @@ def home(request):
     return render(request, 'index.html')
 
 def group(request):
-    return render(request, 'group.html')
+    groups = [
+        {"id":1, "name": 'group1', "desc":"group1 desc"},
+        {"id":2, "name": 'group2', "desc":"group2 desc"},
+        {"id":3, "name": 'group3', "desc":"group3 desc"},
+        {"id":4, "name": 'group4', "desc":"group4 desc"},
+    ]
+    return render(request, 'group.html', {"groups":groups})
+
+def group_by_id(request, key):
+    return HttpResponse(key)
+    # return render(request, 'group.html')
