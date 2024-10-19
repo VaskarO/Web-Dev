70cb95875db96ecda5c57c04c069d0a38761b11c
VaskarO <contact2vo@gmail.com>
2024-10-19 21:21:43 +0200

    db models created

--- Django_app/project02/base/urls.py
+++ Django_app/project02/base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('group/', views.group, name= "group"),
-    path('group/<str:key>', views.group_by_id, name= "group_by_id")
+    # path('group/', views.group, name= "group"),
+    path('group/<str:key>', views.group, name= "group")
 ]
--- Django_app/project02/base/views.py
+++ Django_app/project02/base/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Group
 # Create your views here.
+# groups = [
+#     {"id":1, "name": 'group1', "desc":"group1 desc"},
+#     {"id":2, "name": 'group2', "desc":"group2 desc"},
+#     {"id":3, "name": 'group3', "desc":"group3 desc"},
+#     {"id":4, "name": 'group4', "desc":"group4 desc"},
+# ]
 
 def home(request):
-    return render(request, 'index.html')
+    groups = Group.objects.all()
+    return render(request, 'index.html', {"groups":groups})
 
-def group(request):
-    groups = [
-        {"id":1, "name": 'group1', "desc":"group1 desc"},
-        {"id":2, "name": 'group2', "desc":"group2 desc"},
-        {"id":3, "name": 'group3', "desc":"group3 desc"},
-        {"id":4, "name": 'group4', "desc":"group4 desc"},
-    ]
-    return render(request, 'group.html', {"groups":groups})
+def group(request, key):
+    group = Group.objects.get(id=key)
+    return render(request, 'group.html', {"group":group})
+    # for group in groups:
+    #     if group['id'] == int(key):
+    #         return render(request, 'group.html', {"group":group})
 
-def group_by_id(request, key):
-    return HttpResponse(key)
-    # return render(request, 'group.html')
+# def group_by_id(request, key):
+#     return HttpResponse(key)
+#     # return render(request, 'group.html')
